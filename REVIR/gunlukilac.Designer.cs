70be45d5cd57d0b6576bbe5edd80e5c7b756630f
cep78 <cenapakyildiz@gmail.com>
2023-09-08 16:41:17 +0300

    depo cikis yapildi

--- REVIR/gunlukilac.Designer.cs
+++ REVIR/gunlukilac.Designer.cs
@@ -28,45 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.secToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
             this.Eklebuton = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.Kaydetbuton = new System.Windows.Forms.Button();
             this.odacombobox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secilenhukumluaditextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.secilenhukumluidtextbox = new System.Windows.Forms.TextBox();
+            this.secilenilacidtextbox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.secToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.contextMenuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 26);
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 35);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(396, 313);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.secToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(93, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // secToolStripMenuItem
+            // 
+            this.secToolStripMenuItem.Name = "secToolStripMenuItem";
+            this.secToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.secToolStripMenuItem.Text = "Seç";
+            this.secToolStripMenuItem.Click += new System.EventHandler(this.secToolStripMenuItem_Click);
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.ContextMenuStrip = this.contextMenuStrip2;
             this.dataGridView2.Location = new System.Drawing.Point(494, 35);
             this.dataGridView2.Name = "dataGridView2";
-            this.dataGridView2.Size = new System.Drawing.Size(447, 313);
+            this.dataGridView2.Size = new System.Drawing.Size(246, 313);
             this.dataGridView2.TabIndex = 1;
+            this.dataGridView2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.dataGridView2_MouseDown);
             // 
             // Eklebuton
             // 
-            this.Eklebuton.Location = new System.Drawing.Point(413, 54);
+            this.Eklebuton.Location = new System.Drawing.Point(762, 57);
             this.Eklebuton.Name = "Eklebuton";
             this.Eklebuton.Size = new System.Drawing.Size(75, 53);
             this.Eklebuton.TabIndex = 2;
             this.Eklebuton.Text = "Ekle";
             this.Eklebuton.UseVisualStyleBackColor = true;
+            this.Eklebuton.Click += new System.EventHandler(this.Eklebuton_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(413, 133);
+            this.button2.Location = new System.Drawing.Point(762, 133);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 53);
             this.button2.TabIndex = 3;
@@ -75,7 +111,7 @@
             // 
             // Kaydetbuton
             // 
-            this.Kaydetbuton.Location = new System.Drawing.Point(816, 354);
+            this.Kaydetbuton.Location = new System.Drawing.Point(1028, 370);
             this.Kaydetbuton.Name = "Kaydetbuton";
             this.Kaydetbuton.Size = new System.Drawing.Size(101, 54);
             this.Kaydetbuton.TabIndex = 4;
@@ -88,7 +124,7 @@
             this.odacombobox.FormattingEnabled = true;
             this.odacombobox.Location = new System.Drawing.Point(574, 8);
             this.odacombobox.Name = "odacombobox";
-            this.odacombobox.Size = new System.Drawing.Size(121, 21);
+            this.odacombobox.Size = new System.Drawing.Size(166, 21);
             this.odacombobox.TabIndex = 5;
             // 
             // label1
@@ -96,15 +132,94 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(509, 11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(34, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "label1";
+            this.label1.Text = "Odası";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(855, 35);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(349, 313);
+            this.dataGridView3.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Adi Soyadı";
+            // 
+            // secilenhukumluaditextbox
+            // 
+            this.secilenhukumluaditextbox.Enabled = false;
+            this.secilenhukumluaditextbox.Location = new System.Drawing.Point(76, 374);
+            this.secilenhukumluaditextbox.Name = "secilenhukumluaditextbox";
+            this.secilenhukumluaditextbox.Size = new System.Drawing.Size(100, 20);
+            this.secilenhukumluaditextbox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 410);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Hukumlu id";
+            // 
+            // secilenhukumluidtextbox
+            // 
+            this.secilenhukumluidtextbox.Enabled = false;
+            this.secilenhukumluidtextbox.Location = new System.Drawing.Point(76, 407);
+            this.secilenhukumluidtextbox.Name = "secilenhukumluidtextbox";
+            this.secilenhukumluidtextbox.Size = new System.Drawing.Size(100, 20);
+            this.secilenhukumluidtextbox.TabIndex = 11;
+            // 
+            // secilenilacidtextbox
+            // 
+            this.secilenilacidtextbox.Location = new System.Drawing.Point(588, 371);
+            this.secilenilacidtextbox.Name = "secilenilacidtextbox";
+            this.secilenilacidtextbox.Size = new System.Drawing.Size(152, 20);
+            this.secilenilacidtextbox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(491, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "İlaç id";
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.secToolStripMenuItem1});
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(93, 26);
+            // 
+            // secToolStripMenuItem1
+            // 
+            this.secToolStripMenuItem1.Name = "secToolStripMenuItem1";
+            this.secToolStripMenuItem1.Size = new System.Drawing.Size(92, 22);
+            this.secToolStripMenuItem1.Text = "Sec";
+            this.secToolStripMenuItem1.Click += new System.EventHandler(this.secToolStripMenuItem1_Click);
             // 
             // gunlukilac
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(953, 467);
+            this.ClientSize = new System.Drawing.Size(1345, 607);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.secilenilacidtextbox);
+            this.Controls.Add(this.secilenhukumluidtextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.secilenhukumluaditextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.odacombobox);
             this.Controls.Add(this.Kaydetbuton);
@@ -117,7 +232,10 @@
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.gunlukilac_FormClosed);
             this.Load += new System.EventHandler(this.gunlukilac_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.contextMenuStrip2.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -132,5 +250,16 @@
         private System.Windows.Forms.Button Kaydetbuton;
         private System.Windows.Forms.ComboBox odacombobox;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox secilenhukumluaditextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox secilenhukumluidtextbox;
+        private System.Windows.Forms.TextBox secilenilacidtextbox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem secToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem secToolStripMenuItem1;
     }
 }
--- REVIR/gunlukilac.cs
+++ REVIR/gunlukilac.cs
@@ -17,6 +17,7 @@ namespace REVIR
         {
             InitializeComponent();
             baglanti = new OleDbConnection("Provider=Microsoft.ACE.OleDb.12.0;Data Source=revir.accdb");
+
         }
         OleDbConnection baglanti;
         OleDbCommand komut;
@@ -24,9 +25,27 @@ namespace REVIR
         DataSet ds;
         int secilenilacid;
         int hukumlutabloid;
+        DataTable gridtablo = new DataTable();
         private void Kaydetbuton_Click(object sender, EventArgs e)
         {
+            string sorgu;
 
+            baglanti.Open();
+            for (int i = 0; i < dataGridView3.Rows.Count; i++)
+            {
+
+                string ilacid = dataGridView3.Rows[i].Cells["ilacid"].ToString();
+                string hukumluid = dataGridView3.Rows[i].Cells["hukumluid"].ToString();
+                string ilacadet = dataGridView3.Rows[i].Cells["ilacadet"].ToString();
+                string odaid = dataGridView3.Rows[i].Cells["odaid"].ToString();
+                sorgu = "Insert into depocikis (personelid,ilacid,hukumluid,ilacadet,odaid) values (" + bilgisinif.personelid + "," + ilacid + "," + hukumluid + "," + ilacadet + "," + odaid + ")";
+                komut = new OleDbCommand(sorgu, baglanti);
+                if (komut.ExecuteNonQuery() != 1)//the insert succeded
+                {
+                    MessageBox.Show("Kayıtta Hata Var");
+                    break;
+                }
+            }
         }
 
         private void gunlukilac_FormClosed(object sender, FormClosedEventArgs e)
@@ -43,15 +62,110 @@ namespace REVIR
             odacombobox.ValueMember = "Odaid";
             odacombobox.DisplayMember = "OdaAdi";
             odacombobox.DataSource = dt;
-            da = new OleDbDataAdapter("Select * From ilac", baglanti);
+            da = new OleDbDataAdapter("Select id,cikno,tcno,adisoyadi From hukumlu", baglanti);
             System.Data.DataTable tablo2 = new System.Data.DataTable();
             da.Fill(tablo2);
             dataGridView1.DataSource = tablo2;
+
+            da = new OleDbDataAdapter("Select id,ad,doz From ilac ", baglanti);
+            System.Data.DataTable tablo3 = new System.Data.DataTable();
+            da.Fill(tablo3);
+            dataGridView2.DataSource = tablo3;
+
+
+
+
             baglanti.Close();
 
+           
+            gridtablo.Columns.Add("Hukumlu_id", typeof(int));
+            gridtablo.Columns.Add("Hukumlu_TC", typeof(string));
+            gridtablo.Columns.Add("Hukumlu_Adi_Soyadi", typeof(string));
+
+            gridtablo.Columns.Add("ilac_id", typeof(int));
+            gridtablo.Columns.Add("İlac_Adi", typeof(string));
+            gridtablo.Columns.Add("İlac_Doz", typeof(int));
+
+            gridtablo.Columns.Add("Oda_id", typeof(int));
+            gridtablo.Columns.Add("Oda_Adi", typeof(string));
+
+            gridtablo.Columns.Add("Kullanim_Adeti", typeof(string));
 
 
 
+        }
+
+        private void Eklebuton_Click(object sender, EventArgs e)
+        {
+
+            DataRow satir = gridtablo.NewRow();
+            satir[0]    = secilenhukumluidtextbox.Text;
+            satir[1]    = dataGridView1.SelectedRows[0].Cells["tcno"].Value.ToString();
+            satir[2]    = secilenhukumluaditextbox.Text;
+
+            satir[3]    = dataGridView2.SelectedRows[0].Cells["id"].Value.ToString();
+            satir[4]    = dataGridView2.SelectedRows[0].Cells["ad"].Value.ToString();
+            satir[5]    = dataGridView2.SelectedRows[0].Cells["doz"].Value.ToString();
+
+            satir[6]    = odacombobox.SelectedValue.ToString();
+            satir[7]    = odacombobox.Text.ToString();
+            gridtablo.Rows.Add(satir);
+            dataGridView3.DataSource = gridtablo.DefaultView;
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+          
+            
+        }
+
+        private void dataGridView1_MouseDown(object sender, MouseEventArgs e)
+        {
+            /// mause ile sağ tıklanınca satır seçme kodu
+            if (e.Button == MouseButtons.Right)
+            {
+                int satir = dataGridView1.HitTest(e.X, e.Y).RowIndex;
+                if (satir > -1)
+                {
+                    dataGridView1.Rows[satir].Selected = true;
+                }
+            }
+            /// mause ile sağ tıklanınca satır seçme kodu
+        }
+
+        private void seçToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void secToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            secilenhukumluidtextbox.Text = dataGridView1.SelectedRows[0].Cells["id"].Value.ToString();
+            secilenhukumluaditextbox.Text = dataGridView1.SelectedRows[0].Cells["adisoyadi"].Value.ToString();
+        }
+
+        private void secToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            secilenilacidtextbox.Text = dataGridView2.SelectedRows[0].Cells["id"].Value.ToString();
+        }
+
+        private void dataGridView2_MouseDown(object sender, MouseEventArgs e)
+        {
+            /// mause ile sağ tıklanınca satır seçme kodu
+            if (e.Button == MouseButtons.Right)
+            {
+                int satir = dataGridView2.HitTest(e.X, e.Y).RowIndex;
+                if (satir > -1)
+                {
+                    dataGridView2.Rows[satir].Selected = true;
+                }
+            }
+            /// mause ile sağ tıklanınca satır seçme kodu
         }
     }
 }
